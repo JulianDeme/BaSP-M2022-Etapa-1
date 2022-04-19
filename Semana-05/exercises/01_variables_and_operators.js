e23d53d8cc4bc13ce5547c383ad96d4c2511163c
Julián Demeglio <jdemegliosch@gmail.com>
2022-04-19 04:31:20 -0300

    BaSP-2022: Exercise 01 updated

--- Semana-05/exercises/01_variables_and_operators.js
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -3,19 +3,23 @@
 var numA , numB, totSum;
 numA = 2;
 numB = 3;
+console.log("ValorA:" , numA , "ValorB:" , numB);
 totSum = numA + numB;
+console.log ("Suma:", totSum);
 
 // B.
 
 var string1, string2, sumString;
 string1 = 'Julián';
 string2 = 'Demeglio'
+console.log("String1:", string1, "String2:" , string2);
 sumString = string1 +  string2;
+console.log("Concatenación:" , sumString);
 
 // C.
 
-let length = sumString.length;
-
+totSum = string1.length + string2.length;
+console.log("Letras en strings:" , totSum);
 
 
 
